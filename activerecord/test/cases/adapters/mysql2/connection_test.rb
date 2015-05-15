576f4b4cbe3b685ad3a5178a22a3da19b0b5af9b
Charlie Somerville <charlie@charliesomerville.com>
2015-05-15 16:26:26 +1000

    nuke this test

--- activerecord/test/cases/adapters/mysql2/connection_test.rb
+++ activerecord/test/cases/adapters/mysql2/connection_test.rb
@@ -44,15 +44,6 @@ class MysqlConnectionTest < ActiveRecord::TestCase
     assert_equal [["STRICT_ALL_TABLES"]], result.rows
   end
 
-  def test_mysql_strict_mode_disabled_dont_override_global_sql_mode
-    run_without_connection do |orig_connection|
-      ActiveRecord::Base.establish_connection(orig_connection.merge({:strict => false}))
-      global_sql_mode = ActiveRecord::Base.connection.exec_query "SELECT @@GLOBAL.sql_mode"
-      session_sql_mode = ActiveRecord::Base.connection.exec_query "SELECT @@SESSION.sql_mode"
-      assert_equal global_sql_mode.rows, session_sql_mode.rows
-    end
-  end
-
   def test_mysql_set_session_variable
     run_without_connection do |orig_connection|
       ActiveRecord::Base.establish_connection(orig_connection.deep_merge({:variables => {:default_week_format => 3}}))
