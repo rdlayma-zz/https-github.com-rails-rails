rails/rails

First pass at settings file

activesupport/lib/active_support.rb
@@ -41,6 +41,7 @@ module ActiveSupport
   autoload :ExecutionWrapper
   autoload :Executor
   autoload :FileUpdateChecker
+  autoload :SettingFile
   autoload :EventedFileUpdateChecker
   autoload :ForkTracker
   autoload :LogSubscriber

rdlayma-zz/https-github.com-rails-rails

activesupport/lib/active_support.rb
@@ -35,6 +35,7 @@ module ActiveSupport
 
   autoload :Concern
   autoload :ActionableError
+  autoload :ConfigurationFile
   autoload :CurrentAttributes
   autoload :Dependencies
   autoload :DescendantsTracker